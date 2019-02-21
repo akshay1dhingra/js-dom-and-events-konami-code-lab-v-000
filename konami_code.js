f0f1996efae58268823abfd40066fd05e3f11cce
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-02-21 14:32:52 -0600

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  
+  
+  document.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++;
+      console.log(key)
+
+      if (index === code.length) {
+        alert("You input the Konami code!!!");
+        console.log(key)
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
+
 
 }
+
+init();
